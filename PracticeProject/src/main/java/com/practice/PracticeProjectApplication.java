c4f3402a39cdabdbe74c2046371768aebc2bd5de
Uma <umcgorai@in.ibm.com>
2022-12-02 19:54:47 +0530

    Changed the package name

--- PracticeProject/src/main/java/com/practice/PracticeProjectApplication.java
+++ PracticeProject/src/main/java/com/practice/PracticeProjectApplication.java
@@ -1,4 +1,4 @@
-package com.practice.PracticeProject;
+package com.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
