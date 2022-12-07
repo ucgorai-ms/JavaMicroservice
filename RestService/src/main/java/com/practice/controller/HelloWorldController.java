b0767b0b94403a79d3ed2dc4c715ed783f97fabc
Uma <umcgorai@in.ibm.com>
2022-12-07 20:52:09 +0530

    Added i18n code

--- RestService/src/main/java/com/practice/controller/HelloWorldController.java
+++ RestService/src/main/java/com/practice/controller/HelloWorldController.java
@@ -1,5 +1,9 @@
 package com.practice.controller;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +14,13 @@ import com.practice.bean.HelloWorldBean;
 
 @RestController
 public class HelloWorldController {
+	
+	private MessageSource messageSource;
+	
+
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 
 	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
 	public String getHelloWorld() {
@@ -29,6 +40,14 @@ public class HelloWorldController {
 		
 		return new HelloWorldBean(String.format("Hello %s, Welcome to this project!!", name));
 		
+	}
+	
+	@GetMapping(path = "/hello-world-internationalized")
+	public String getHelloWorldInternationalized() {
+		
+		Locale locale = LocaleContextHolder.getLocale();
+		return this.messageSource.getMessage("goodmorning.message", null, "Good Morning", locale);
+
 	}
 
 }
